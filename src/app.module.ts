2cdcd84e9041e66df93833f3cdba58e21b9d9889
geby9469 <bjk9406@gmail.com>
2024-03-19 18:23:43 +0900

    initial commit and nestjs typeorm crud

---
+++ src/app.module.ts
@@ -0,0 +1,29 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users/user.entity';
+import { UsersModule } from './users/users.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env.local',
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE_NAME,
+      entities: [User], // let module know about User entity
+      synchronize: true,
+    }),
+    UsersModule,
+  ],
+  providers: [AppService],
+  controllers: [AppController],
+})
+export class AppModule {}
---
+++ src/users/user.entity.ts
@@ -0,0 +1,18 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+// Create 'User' entity
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  firstName: string;
+
+  @Column()
+  lastName: string;
+
+  @Column({ default: true })
+  isActive: boolean;
+}
